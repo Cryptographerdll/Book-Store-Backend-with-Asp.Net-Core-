547a05a7b8d14f5e73b91309c128c9c40187724c
Cryptographer <85350190+CryptographyHacker@users.noreply.github.com>
2022-01-13 20:58:10 +0100

    Add files via upload

---
+++ BookRepository.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models.Repositories
+{
+    public class BookRepository : IbookStoreRepositorie <Book>
+    {
+        List <Book> Books;
+
+        public BookRepository()
+        {
+            Books = new List<Book>()
+            {
+                new Book
+                {
+                    Id = 1,
+                    Title = "C# in Depth",
+                    Description = "How to Master C# in Good way for Beginners "
+                },
+
+                new Book
+                {
+                    Id = 1,
+                    Title = "Eloquent Javascript",
+                    Description = "How to Master Js Libraries for Beginners "
+                },
+
+                new Book
+                {
+                    Id = 1,
+                    Title = "Ruby on Rails",
+                    Description = "Get started with Ruby Language "
+                }
+            };
+        }
+
+        public void Add(Book entity)
+        {
+            Books.Add(entity);
+        }
+
+        public void Delete(int Id)
+        {
+            var Book = Find(Id);
+
+            Books.Remove(Book);
+        }
+
+        public Book Find(int Id)
+        {
+            var book = Find(Id);
+
+            return book;
+        }
+
+        public IList<Book> List()
+        {
+            return Books;
+        }
+
+        public void Update(int Id, Book newBook)
+        {
+            var Book = Books.SingleOrDefault (b => b.Id == Id);
+
+            Book.Title = newBook.Title;
+            Book.Description = newBook.Description;
+            Book.Author = newBook.Author;
+        }
+    }
+}
---
+++ IbookStoreRepositorie.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookStore.Models.Repositories
+{
+    interface IbookStoreRepositorie <TEntity>
+    {
+        IList <TEntity> List();
+
+        TEntity Find(int Id);
+
+        void Add(TEntity entity);
+
+        void Update(int Id, TEntity entity);
+
+        void Delete(int Id);
+    }
+}
